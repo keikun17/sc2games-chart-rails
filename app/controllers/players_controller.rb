fb1fd2317041d3d9ff3ea7b00863f99c3dd7dff0
Buddy Magsipoc <keikun17@gmail.com>
2015-11-25 00:29:06 +0800

    Match date extended format。

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,7 +1,14 @@
 class PlayersController < ApplicationController
   def match_history
-    @player  = Bnet::Starcraft2::Profile.find(region: 'us', profile_id: 2143215, name: 'PlayerOne')
+    @dates = []
+
+    # @player  = Bnet::Starcraft2::Profile.find(region: 'us', profile_id: 2143215, name: 'PlayerOne')
+    @player  = Bnet::Starcraft2::Profile.find(region: params[:region], profile_id: params[:profile_id], name: params[:name])
     @matches =  @player.matches
+
+    @matches.each do |match|
+      match.date = Time.at(match.date).strftime('%F')
+    end
 
     render json: @matches
   end
