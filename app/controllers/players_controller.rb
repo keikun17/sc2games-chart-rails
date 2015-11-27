2719f4fa1bfc1b4fbcc553aedb755185f3a07cb8
Buddy Magsipoc <keikun17@gmail.com>
2015-11-27 23:45:22 +0800

    Spit Profile not found errors。

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -10,28 +10,33 @@ class PlayersController < ApplicationController
 
   def match_history
     @dates = []
-
     @profile  = Bnet::Starcraft2::Profile.find(region: params[:region], profile_id: params[:profile_id], name: params[:name])
+
     if @profile
       Player.find_or_create_by(name: params[:name], region: params[:region], profile_id: params[:profile_id])
-    end
-    @matches =  @profile.matches
 
-    @dates = {}
-    (1.year.ago.beginning_of_day.to_datetime.to_i .. Time.now.beginning_of_day.to_datetime.to_i).step(1.day) do |date|
-      date = Time.at(date).strftime("%Y-%m-%d")
-      @dates[date] = []
-    end
+      @matches =  @profile.matches
 
-    @matches.each do |match|
-      match_date = Time.at(match.date).strftime('%F')
-
-      if !@dates[match_date].nil?
-        # If match date is still within the year, add the match record
-        @dates[match_date] << match
+      @dates = {}
+      (1.year.ago.beginning_of_day.to_datetime.to_i .. Time.now.beginning_of_day.to_datetime.to_i).step(1.day) do |date|
+        date = Time.at(date).strftime("%Y-%m-%d")
+        @dates[date] = []
       end
+
+      @matches.each do |match|
+        match_date = Time.at(match.date).strftime('%F')
+
+        if !@dates[match_date].nil?
+          # If match date is still within the year, add the match record
+          @dates[match_date] << match
+        end
+      end
+
+      render json: {player: @profile, matches: @dates}, callback: params['callback']
+
+    else
+      render json: {error: "Profile not found"}
     end
 
-    render json: {player: @profile, matches: @dates}, callback: params['callback']
   end
 end
